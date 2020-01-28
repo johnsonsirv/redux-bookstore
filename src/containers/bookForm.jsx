68f3398e433263e7917743ad6a1788530076c6ac
Victor Johnson <johnsonsirv@gmail.com>
2020-01-28 22:04:48 +0100

    make BookForm stateful, to store title, category, handle events

--- src/containers/bookForm.jsx
+++ src/containers/bookForm.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BookForm = () => {
-  const categories = [
-    { id: 1, name: 'Action' },
-    { id: 2, name: 'Biography' },
-    { id: 3, name: 'History' },
-    { id: 4, name: 'Horror' },
-    { id: 5, name: 'Kids' },
-    { id: 6, name: 'Learning' },
-    { id: 7, name: 'Sci-Fi' },
-  ];
-  return (
-    <form>
-      <input type="text" placeholder="book title" />
-      <select>
-        {categories.map(c => (
-          <option key={c.id} value={c.name}>
-            {c.name}
-          </option>
-        ))}
-      </select>
-      <input type="submit" value="Save(Mileston 3)" />
-    </form>
-  );
-};
+class BookForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      book: { title: '', category: '' },
+      categories: [
+        { id: 0, name: ' ' },
+        { id: 1, name: 'Action' },
+        { id: 2, name: 'Biography' },
+        { id: 3, name: 'History' },
+        { id: 4, name: 'Horror' },
+        { id: 5, name: 'Kids' },
+        { id: 6, name: 'Learning' },
+        { id: 7, name: 'Sci-Fi' },
+      ],
+    };
+  }
+
+  handleChange = ({ currentTarget: input }) => {
+    const { book } = this.state;
+    book[input.name] = input.value;
+    this.setState({ book });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { book } = this.state;
+  };
+
+  render() {
+    const {
+      book: { title },
+      categories,
+    } = this.state;
+
+    return (
+      <form>
+        <input
+          name="title"
+          value={title}
+          type="text"
+          onChange={this.handleChange}
+          placeholder="book title"
+        />
+        <select onChange={this.handleChange} name="category">
+          {categories.map(c => (
+            <option key={c.id} value={c.name}>
+              {c.name}
+            </option>
+          ))}
+        </select>
+        <button type="button" onClick={this.handleSubmit}>
+          Save
+        </button>
+      </form>
+    );
+  }
+}
 
 export default BookForm;
